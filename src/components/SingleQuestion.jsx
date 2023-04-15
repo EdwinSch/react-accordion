d74861904b9b1e1d97024b7b5e6e378f6e14bab5
front-ed <edwinschutjes@gmail.com>
2023-04-15 21:53:07 +0200

    add: toggle logic and buttons

--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const SingleQuestion = ({ title, info }) => {
+  //
   const [showInfo, setShowInfo] = useState(false);
+
   return (
     <article className="question">
       <header>
         <h5>{title}</h5>
+        <button className="question-btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
       </header>
-      <p>{info}</p>
+      {showInfo && <p>{info}</p>}
     </article>
   );
 };
